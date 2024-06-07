4e7750db9cb3dfec8b391660dd8f8b6c79ce226a
Caio-MacBook <caioa.morais@gmail.com>
2024-06-07 11:14:37 -0300

    [FEAT] mongoose connection

--- middlewares/connect-db.ts
+++ middlewares/connect-db.ts
@@ -1,22 +1,25 @@
-import type {NextApiRequest, NextApiHandler, NextApiResponse} from 'next';
-import mongoose from 'mongoose';
+import type { NextApiRequest, NextApiHandler, NextApiResponse } from 'next'
+import mongoose from 'mongoose'
 
-export const connectMongoDB =  (handler: NextApiHandler) => {
-  async (req: NextApiRequest, res: NextApiResponse) => {
+export const connectMongoDB = (handler: NextApiHandler) => {
+  let connect = async (req: NextApiRequest, res: NextApiResponse) => {
     //check connection
-    if(mongoose.connections[0].readyState){
-      return handler(req, res);
+    if (mongoose.connections[0].readyState) {
+      return handler(req, res)
     }
 
-    const {DB_CONNECTION_STRING} = process.env;
-    if(!DB_CONNECTION_STRING){
-      return res.status(500).json({error:'DB Connection Error'})
+    const { DB_CONNECTION_STRING } = process.env
+    if (!DB_CONNECTION_STRING) {
+      return res.status(500).json({ error: 'DB Connection Error' })
     }
 
-    await mongoose.connect(DB_CONNECTION_STRING);
-    mongoose.connection.on('connected', () => console.log('DB Connection stablished'))
+    mongoose.connection.on('connected', () =>
+      console.log('DB Connection stablished')
+    )
     mongoose.connection.on('error', () => console.log('DB Connection Error'))
+    await mongoose.connect(DB_CONNECTION_STRING)
 
     return handler(req, res)
-    }
+  }
+  return connect
 }
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,14 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { connectMongoDB } from '../../middlewares/connect-db'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const {login, senha} = req.body
-    if (login === 'admin@admin.com' && 
-        senha === 'Admin@123'
-    ){
-      res.status(200).json({error: 'Usuário logado com sucesso!'})
+    const { login, senha } = req.body
+    if (login === 'admin@admin.com' && senha === 'Admin@123') {
+      res.status(200).json({ error: 'Successfully logged in!' })
     }
-    return res.status(400).json({error: 'Usuário ou senha inválidos.'})
+    return res.status(400).json({ error: 'Failed to log in.' })
   }
-  return res.status(405).json({ error: 'Metodo informado nao é valido' })
+  return res.status(405).json({ error: 'Unsupported Method' })
 }
+export default connectMongoDB(handler)
